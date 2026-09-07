dfc6be454802e2a69d341bd67f429be6e58d66a8
Kiruthika <kiruthikak.offl@gmail.com>
2026-09-07 21:59:25 +0530

    Valid Palindrome

---
+++ Java/Valid Palindrome.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(Character.isLetterOrDigit(s.charAt(i)))
+            {
+                str=str+s.charAt(i);
+            }
+        }
+        str=str.toLowerCase();
+        int left=0;
+        int right=str.length()-1;
+        if(str.length()<=1)
+        {
+            return true;
+        }
+        while(left<=right)
+        {
+            if(str.charAt(left)==str.charAt(right))
+            {
+            
+            left++;
+            right--;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+return true;        
+    }
+}
