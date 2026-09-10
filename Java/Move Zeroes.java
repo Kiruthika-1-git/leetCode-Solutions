4cdcc6151a71f6af413ff9d8bbc76b2e9814e22c
Kiruthika <kiruthikak.offl@gmail.com>
2026-09-10 21:48:03 +0530

    Move Zeroes

---
+++ Java/Move Zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left=0;
+        int right=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[right]==0)
+            {
+                right++;
+            }
+            else
+            {
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+                right++;
+            }
+        }
+    }
+}
